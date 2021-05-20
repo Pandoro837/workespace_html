fbdd290c098a7355f10d976c4f5b7d256f754e15
young <mould837@naver.com>
2021-05-20 18:11:22 +0900

    0520

---
+++ chapter01/src/com/javaex/ex01/Ex01.java
@@ -0,0 +1,64 @@
+package com.javaex.ex01;
+
+public class Ex01 
+{
+	public static void main(String[] agrs)
+	{
+//		바이트형 예제
+//		byte myAge;
+//		myAge = 25;
+//		System.out.println(myAge);
+		
+		
+//		int myAge;
+//		myAge = 129;	//범위에서 벗어나는 값을 넣어본다 --> 해결 방법은
+//		System.out.println(myAge);					자료형을 변경한다
+												/*long 형일 때, L을 넣어준다*/ 
+		
+//		변수가 여러개일 때, 표현하는 방법
+		
+//		int age; 		변수 생성과 값 입력되는 순서 비교
+//		int weight;
+//		int tall;
+//		
+//		age = 25;
+//		weight = 78;
+//		tall = 180;
+		
+//		int age; 		변수 생성과 값 입력되는 순서 비교
+//		int weight;
+//		int tall;
+//		
+//		
+//		weight = 78;
+//		tall = 180;
+//		age = 25;
+//		
+		
+//		int age;
+//		age = 25;
+//						상황에 따라 변수에 대한 값을 나중에 넣어야 할 경우도
+//						있다, 하지만 둘은 같은 표현이다
+//		int age = 25;
+		
+//		변수의 자료형이 같을 경우
+		
+//		int var01	
+//		int var02	=	int var01, var02, var03;
+//		int var03
+		
+//		변수형과 값을 함께 입력하며
+//		 한 꺼번에 넣는 것도 가능하다
+
+		int var01=10, var02=20, var03=30; 
+		
+		int number = 009;
+//		정수 값에 0으로 시작하는 수를 넣으면 인식하지 못한다
+		System.out.println(number);		
+		System.out.println(var01);	
+		System.out.println(var02);	
+		System.out.println(var03);	
+		
+		
+	}
+}
---
+++ chapter01/src/com/javaex/ex01/Ex04.java
@@ -0,0 +1,29 @@
+package com.javaex.ex01;
+
+public class Ex04 
+{
+	public static void main(String[] args)
+	{
+//		기본 사용 방법
+//		char ch01, ch02
+//		ch01 = 'A';
+//		ch02 = '안';
+//		System.out.println(ch01);
+//		System.out.println(ch02);
+		
+//		char ch03 = 65;
+//		System.out.println(ch03);
+		
+//		문자 - 한 글자 char - a, b 등 하나만 
+//		문자열 - 한 글자 이상
+//		보통은 문자열을 사용하여 지정한다	
+		
+		char ch05 = '최';
+		String ch06 = "안녕하세요";
+		
+		System.out.println(ch05);
+		System.out.println(ch06);
+		
+		
+	}
+}
